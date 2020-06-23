829d88e6b59ad143b4fba198f54e0fce2296881d
Jerry Peeler <jerry11peeler@gmail.com>
2020-06-23 12:44:43 -0400

    thru section 10 - initial migration

--- Models/Page.cs
+++ Models/Page.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CmsShoppingCart.Models
 {
     public class Page
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Slug { get; set; }
+
+        [Required]
         public string Context { get; set; }
         public int Sorting { get; set; }
     }
